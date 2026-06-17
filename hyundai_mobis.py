0960d8e19815f5f0b1f5a86beda71d5156e83b18
Woo-Jinsung <woojinsung1110@korea.ac.kr>
2026-06-17 22:17:40 +0900

    Update hyundai_mobis(Prob1)

--- hyundai_mobis.py
+++ hyundai_mobis.py
@@ -1,3 +1,10 @@
 #한화에어로스페이스 0329
 #Python3으로 총 5문제 180분
 
+#1
+def solution(n, m, features):
+    answers = []
+    for feature in features:
+        answers.append(feature[0] * n + feature[1] * m)
+    answers.sort()
+    return answers[0]
