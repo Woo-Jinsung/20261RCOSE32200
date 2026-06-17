65066b108621a053eb09bf657205b922b4a8bbca
Woo-Jinsung <woojinsung1110@korea.ac.kr>
2026-06-17 22:07:40 +0900

    Update hanwha_aerospace(Prob2)

--- hanwha_aerospace.py
+++ hanwha_aerospace.py
@@ -11,3 +11,41 @@ def solution(n, k, menu):
     ans.sort()
     return sum(ans[:k])
 
+#2
+def findlog(num):
+    ans = 0
+    while num >= 10:
+        num = num // 10
+        ans += 1
+    return ans
+
+def solution(initdb, mindb, maxdb, commands):
+    answer = []
+    now = initdb
+    for command in commands:
+        if command[0] == 0:
+            target = findlog(command[1]) * 10
+            if now + target >= maxdb:
+                now = maxdb
+            else:
+                now += target
+        elif command[0] == 1:
+            target = findlog(command[1]) * 10
+            if now - target <= mindb:
+                now = mindb
+            else:
+                now -= target
+        elif command[0] == 2:
+            if now + command[1] >= maxdb:
+                now = maxdb
+            else:
+                now += command[1]
+        elif command[0] == 3:
+            if now - command[1] <= mindb:
+                now = mindb
+            else:
+                now -= command[1]
+        
+        answer.append(now)
+    
+    return answer
