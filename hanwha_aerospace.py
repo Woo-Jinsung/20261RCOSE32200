1dde3647132231962591cbb87f0fa795cb3fd97d
Woo-Jinsung <woojinsung1110@korea.ac.kr>
2026-06-17 22:04:29 +0900

    Update hanwha_aerospace(Prob1)

--- hanwha_aerospace.py
+++ hanwha_aerospace.py
@@ -1,3 +1,13 @@
 #한화에어로스페이스 0419
 #Python3으로 총 4문제 120분
 
+#1
+def solution(n, k, menu):
+    ans = []
+    for i in range(n):
+        temp = menu[i][0] * 4 + menu[i][1] * 4 + menu[i][2] * 9
+        ans.append(temp)
+    
+    ans.sort()
+    return sum(ans[:k])
+
