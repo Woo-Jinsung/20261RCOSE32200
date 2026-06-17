03459e1920fdd531cb5eeaf16645534acdda56d6
Woo-Jinsung <woojinsung1110@korea.ac.kr>
2026-06-17 21:53:21 +0900

    Update hanwha_system_defense(Prob1)

--- hanwha_system_defense.py
+++ hanwha_system_defense.py
@@ -1,2 +1,16 @@
 #한화시스템/방산 260425
 #Python3으로 총 3문제 120분
+
+#1
+def solution(fishes, feeds):
+    fishes.sort(reverse=True)
+    n = len(feeds)
+    for i in range(n):
+        feed = feeds[i]
+        for fish in fishes:
+            if fish <= feed:
+                feed -= fish
+            else:
+                fishes.pop()
+
+    return len(fishes)
