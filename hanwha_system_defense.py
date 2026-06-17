c3aabb3091b110d9d0c319b1a6ad63a4036b3303
Woo-Jinsung <woojinsung1110@korea.ac.kr>
2026-06-17 21:56:10 +0900

    Update hanwha_system_defense(Prob2)

--- hanwha_system_defense.py
+++ hanwha_system_defense.py
@@ -14,3 +14,25 @@ def solution(fishes, feeds):
                 fishes.pop()
 
     return len(fishes)
+
+#2
+def solution(square, k):
+    dp = [[] for _ in range(11)]
+    dp[1] = square
+    now = 1
+    while now <= k:
+        start = [row.copy() for row in dp[now]]
+        n = len(start)
+        temp = [[] * (2 * n) for _ in range(2 * n)]
+
+        for i in range(n):
+            temp[i] = start[i]
+            for j in range(n - 1, -1, -1):
+                temp[i].append(start[i][j])
+            temp[2 * n - i - 1] = temp[i]
+        
+        now += 1
+        dp[now] = temp
+    
+    return dp[k + 1]
+
